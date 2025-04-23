61519e09f5c9e212dc881aa839c9a87d5f7a18f7
inaflux <jhen9620@gmail.com>
2025-04-22 20:53:12 -0400

    Added a calculator

---
+++ ConsoleApp.ConditonsAndDecisions/Program.cs
@@ -0,0 +1,119 @@
+﻿namespace ConsoleApp.ConditonsAndDecisions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prompt Input
+            Console.Write("Please enter the student grade: ");
+            //Global Variable/ Global Scope
+            int grade = Convert.ToInt32(Console.ReadLine());
+
+            //Decide pass or fail based on user input
+
+            if (grade > 50)
+            {
+                Console.WriteLine("Student has passed the class!!!! WOOT WOOT! ");
+            }
+            else
+            {
+                Console.WriteLine("Student has failed...WOMP WOMP WOMPPPPP");
+            }
+            //More Complex if else statements
+
+            Console.WriteLine("********* Complex IF ELSE **********");
+            Console.WriteLine();
+
+            if(grade < 0 || grade > 100)
+            {
+                Console.WriteLine("Invalid input entered");
+            }
+
+            else if (grade < 50)
+            {
+                Console.WriteLine("Get good dawg, F!");
+            }
+
+            else if(grade >= 51 && grade <= 64)
+            {
+                Console.WriteLine("D");
+            }
+            else if (grade >= 76 && grade <= 85)
+            {
+                Console.WriteLine("C");
+            }
+            else if (grade >= 86 && grade <= 95)
+            {
+                Console.WriteLine("B");
+            }
+            else if (grade >= 96 && grade <= 100)
+            {
+                Console.WriteLine("A");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("********* Complex IF ELSE **********");
+
+
+
+
+            //Ternary Operator
+
+            string passStatus = grade < 50 ? "Fail" : "Pass";
+
+            //Switch Statements
+
+
+
+
+            Console.WriteLine("********* Switch Statement**********");
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter the day of the week: ");
+            int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+            switch (dayOfWeek)
+            {
+               
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+
+
+            }
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("********* End Switch Statment**********");
+        }
+
+
+        
+
+
+    }
+}
---
+++ ConsoleApp.SimpleCalc/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace ConsoleApp.SimpleCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********** Simple Calculator ************");
+            Console.WriteLine();
+
+            Console.Write("Please enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.Write("Please enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            int sum = num1 + num2;
+
+            //Multiply numbers
+
+            int product = num1 * num2;
+
+            //Divide numbers
+
+            int quotient = num1 / num2;
+
+            //Subtract numbers
+            int difference = num1 - num2;
+
+            //Modulus 
+            int mod = num1 % num2;
+
+
+            //Show Calculator Options
+            Console.WriteLine("Welcome to the simple calculator. Here are your options: ");
+            Console.WriteLine("Enter A for addition");
+            Console.WriteLine("Enter S for subtraction");
+            Console.WriteLine("Enter M for multiplication");
+            Console.WriteLine("Enter D for division");
+            Console.WriteLine("Enter R for modulous");
+            string operationChoice = Console.ReadLine();
+
+            //Decide which opertation is needed based on selected option
+            if (operationChoice == "A" )
+            {
+                Console.Write($"You chose to add {num1} to {num2} and the answer is: {sum}");
+            }
+
+            else if (operationChoice == "S")
+            {
+                Console.Write($"You chose to subtract {num1} from {num2} and the answer is: {difference}");
+            }
+            else if (operationChoice == "M")
+            {
+                Console.Write($"You chose to multiply {num1} by {num2} and the answer is: {product}");
+            }
+            else if (operationChoice == "D")
+            {
+                Console.Write($"You chose to divide {num1} by {num2} and the answer is: {quotient}");
+            }
+            else if (operationChoice == "R")
+            {
+                Console.Write($"You chose to find the modulus of  {num1} / {num2} and the answer is: {mod}");
+            }
+            //Print output
+            Console.WriteLine();
+            Console.WriteLine("*********** Simple Calculator ************");
+        }
+    }
+}
